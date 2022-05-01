544e51a5009ababf7f7cac76825b55db6da17e5a
Ratchgamer123 <trautwein.maximilian04@gmail.com>
2022-05-02 00:19:37 +0200

    1.0

--- Assets/Scripts/WallRun.cs
+++ Assets/Scripts/WallRun.cs
@@ -9,23 +9,23 @@ public class WallRun : MonoBehaviour
     [SerializeField] private Transform orientation;
 
     [Header("Wall Running")]
-    [SerializeField] private float wallDistance = 0.5f;
+    [SerializeField] private float wallDistance = 0.6f;
     [SerializeField] private float minimumJumpHeight = 1.5f;
 
     [Header("Wall Running")]
-    [SerializeField] private float wallRunGravity = 0.5f;
-    [SerializeField] private float wallRunJumpForce = 6f;
+    [SerializeField] private float wallRunGravity = 0.2f;
+    [SerializeField] private float wallRunJumpForce = 15.0f;
     [SerializeField] private LayerMask walls;
 
     [Header("Camera Effects")]
     public Camera cam;
-    public float fov = 90f;
-    public float wallRunfov = 110f;
-    public float wallRunfovTime = 20f;
-    [SerializeField] private float camTilt = 20f;
-    [SerializeField] private float camTiltTime = 20f;
+    public float fov = 90.0f;
+    public float wallRunfov = 110.0f;
+    public float wallRunfovTime = 20.0f;
+    [SerializeField] private float camTilt = 15.0f;
+    [SerializeField] private float camTiltTime = 25.0f;
     [SerializeField] private float chromAb = 0.1f;
-    [SerializeField] private float wallRunChromAb = 0.2f;
+    [SerializeField] private float wallRunChromAb = 1.0f;
     public Volume volume;
 
     public float tilt { get; private set; }
